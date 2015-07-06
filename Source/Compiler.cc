5abf66163612f29e5815f40ac8e5e1daff2ed813
Justin Harrison <justin@matthin.com>
2015-07-06 16:59:54 -0400

    Compile h2, and h3

--- Source/Compiler.cc
+++ Source/Compiler.cc
@@ -11,8 +11,19 @@ std::string Compiler::toHTML() const {
   std::string html;
 
   for (auto token : tokens) {
-    if (token.operation == Operation::H1) {
+    switch (token.operation) {
+    case Operation::H1:
       html += "<h1>" + token.text + "</h1>";
+      break;
+    case Operation::H2:
+      html += "<h2>" + token.text + "</h2>";
+      break;
+    case Operation::H3:
+      html += "<h3>" + token.text + "</h3>";
+      break;
+    default:
+      // Should log a message saying the operation isn't supported yet.
+      break;
     }
   }
 
--- Tests/Compiler.cc
+++ Tests/Compiler.cc
@@ -1,10 +1,14 @@
 #include "Catch.h"
 #include "Compiler.h"
 
-TEST_CASE("Compiles to html") {
+TEST_CASE("Compiles headers") {
   const lu::Compiler compiler(std::vector<lu::Token> {
-    {lu::Token(lu::Operation::H1, " Hello")}
+    {lu::Token(lu::Operation::H1, " Hello")},
+    {lu::Token(lu::Operation::H2, " Hello")},
+    {lu::Token(lu::Operation::H3, " Hello")}
   });
-  REQUIRE(compiler.toHTML() == "<h1> Hello</h1>");
+  REQUIRE(
+    compiler.toHTML() == "<h1> Hello</h1><h2> Hello</h2><h3> Hello</h3>"
+  );
 }
 
