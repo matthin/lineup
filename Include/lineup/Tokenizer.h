3c0e0081b5ebfe21dc175ffc58cc43476ac4b1b6
Justin Harrison <justin@matthin.com>
2015-07-09 11:31:58 -0400

    Split token type into seperate method

--- Include/lineup/Tokenizer.h
+++ Include/lineup/Tokenizer.h
@@ -12,6 +12,9 @@ public:
   Tokenizer(const std::string& markdown);
   std::vector<Token> tokenize(const std::string& markdown) const;
   const std::vector<Token> tokens;
+
+private:
+  Operation detectType(const std::string& line) const;
 };
 
 } // namespace lu
--- Source/Tokenizer.cc
+++ Source/Tokenizer.cc
@@ -15,41 +15,60 @@ std::vector<Token> Tokenizer::tokenize(const std::string& markdown) const {
   std::istringstream stream(markdown);
   std::string line;
   while (std::getline(stream, line)) {
-    if (line.at(0) == '#') {
-      if (line.at(1) == '#') {
-        if (line.at(2) == '#') {
-          tokens.push_back(
-            Token(Operation::H3, line.substr(3))
-          );
-        } else {
-          tokens.push_back(
-            Token(Operation::H2, line.substr(2))
-          );
-        }
-      } else {
-        tokens.push_back(
-          Token(Operation::H1, line.substr(1))
-        );
-      }
-    } else if (
-        // I need to automatically check if the char is numeric.
-        (line.at(0) == '1' || line.at(0) == '2') && line.at(1) == '.'
-    ) {
-      tokens.push_back(
-        Token(Operation::OL, line.substr(2))
-      );
-    } else if (line.at(0) == '*') {
-      tokens.push_back(
-        Token(Operation::UL, line.substr(1))
-      );
-    } else {
-      tokens.push_back(
-        Token(Operation::P, line)
-      );
+    const auto type = detectType(line);
+    std::string text;
+    switch(type) {
+    case Operation::H1:
+      text = line.substr(1);
+      break;
+    case Operation::H2:
+      text = line.substr(2);
+      break;
+    case Operation::H3:
+      text = line.substr(3);
+      break;
+    case Operation::OL:
+      text = line.substr(2);
+      break;
+    case Operation::UL:
+      text = line.substr(1);
+      break;
+    case Operation::P:
+      text = line;
+      break;
     }
+
+    tokens.push_back(Token(type, text));
   }
 
   return tokens;
+}
+
+Operation Tokenizer::detectType(const std::string& line) const {
+  Operation type;
+
+  if (line.at(0) == '#') {
+    if (line.at(1) == '#') {
+      if (line.at(2) == '#') {
+        type = Operation::H3;
+      } else {
+        type = Operation::H2;
+      }
+    } else {
+      type = Operation::H1;
+    }
+  } else if (
+      // I need to automatically check if the char is numeric.
+      (line.at(0) == '1' || line.at(0) == '2') && line.at(1) == '.'
+  ) {
+    type = Operation::OL;
+  } else if (line.at(0) == '*') {
+    type = Operation::UL;
+  } else {
+    type = Operation::P;
+  }
+
+  return type;
 }
 
 } // namespace lu
